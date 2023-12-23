af7076eed2e4251aa313eedde9b862f235912a47
Ricardo Henrique Silva Rodrigues <135380624+1RiKaS1@users.noreply.github.com>
2023-12-22 21:08:10 -0300

    Jogo Lindo,Css Tabuleiro Implementado

--- src/pages/Multiplayer/index.jsx
+++ src/pages/Multiplayer/index.jsx
@@ -3,11 +3,17 @@ import Button from "../../components/Button";
 import "../../components/Estilo.modules.css";
 import { useState } from "react";
 
+import './single.css'
+
 export const Multiplayer = () => {
 
     return (
-            <div>
-                <Tabuleiro />
+        <>
+            <div style={{fontFamily:'pixel'}}  className="Objetos">
+                <h1 className="P1">Pato  </h1>
+                <h1 className="P2">Pato  </h1>
             </div>
+            <Tabuleiro />
+        </>
     )
 }
--- src/pages/Singleplayer/index.jsx
+++ src/pages/Singleplayer/index.jsx
@@ -2,11 +2,18 @@ import Tabuleiro from "../../components/Tabuleiro";
 import Button from "../../components/Button";
 import { useState } from "react";
 
+// Arquivo Css da Página
+import './single.css'
+
 export const Singleplayer = () => {
 
     return (
-            <div>
+            <>
+                <div style={{fontFamily:'pixel'}} className="Objetos">
+                    <h1 className="P1">Pato Trainee</h1>
+                    <h1 className="P2">Pato Efetivo</h1>
+                </div>
                 <Tabuleiro />
-            </div>
+            </>
     )
 }
